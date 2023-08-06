1eac7b54966b1576fea0205dea5ce44b56da8e4d
OlaleyeFisayo <91983317+OlaleyeFisayo@users.noreply.github.com>
2023-08-06 16:38:08 +0100

    Update Navbar component

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -7,51 +7,48 @@ export default function Navbar() {
     <div className="nav-container">
       <nav id="desktop-view">
         <img src={logo} alt="Logo" loading="lazy" />
-        <div>
-          <div className="line"></div>
-          <ul className="nav-list">
-            <li>
-              <NavLink
-                className={({ isActive }) => (isActive ? "active" : "")}
-                to=".."
-              >
-                <p>
-                  <span className="bold">01</span>Home
-                </p>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                className={({ isActive }) => (isActive ? "active" : "")}
-                to="/destination"
-              >
-                <p>
-                  <span className="bold">02</span>Destination
-                </p>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                className={({ isActive }) => (isActive ? "active" : "")}
-                to="/crew"
-              >
-                <p>
-                  <span className="bold">03</span>crew
-                </p>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                className={({ isActive }) => (isActive ? "active" : "")}
-                to="/technology"
-              >
-                <p>
-                  <span className="bold">04</span>Technology
-                </p>
-              </NavLink>
-            </li>
-          </ul>
-        </div>
+        <ul className="nav-list">
+          <li>
+            <NavLink
+              className={({ isActive }) => isActive ? "active" : ""}
+              to=".."
+            >
+              <p>
+                <span className="bold">01</span>Home
+              </p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive }) => isActive ? "active" : ""}
+              to="/destination"
+            >
+              <p>
+                <span className="bold">02</span>Destination
+              </p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive }) => isActive ? "active" : ""}
+              to="/crew"
+            >
+              <p>
+                <span className="bold">03</span>crew
+              </p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive }) => isActive ? "active" : ""}
+              to="/technology"
+            >
+              <p>
+                <span className="bold">04</span>Technology
+              </p>
+            </NavLink>
+          </li>
+        </ul>
       </nav>
     </div>
   );
