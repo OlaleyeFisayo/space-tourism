fc30fc6ba344476fe7d968c0d5bf694b0123a2ed
OlaleyeFisayo <91983317+OlaleyeFisayo@users.noreply.github.com>
2023-08-18 14:32:09 +0100

    Basic Setup

--- src/pages/technology/index.tsx
+++ src/pages/technology/index.tsx
@@ -5,6 +5,11 @@ export default function Technology() {
   return (
     <section id="technology">
       <Navbar />
+      <section className="technology-content">
+        <h1 className="header">
+          <span className="number">03</span>Space Lanuch 101
+        </h1>
+      </section>
     </section>
   );
 }
