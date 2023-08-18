88c89610bf76a7347b97b0619087da207db78e59
OlaleyeFisayo <91983317+OlaleyeFisayo@users.noreply.github.com>
2023-08-18 15:52:41 +0100

    Completed Technology page

--- src/components/technologies/index.tsx
+++ src/components/technologies/index.tsx
@@ -1,32 +1,56 @@
-import imgLandscape from "../../assets/technology/image-launch-vehicle-landscape.jpg";
-import imgPortrait from "../../assets/technology/image-launch-vehicle-portrait.jpg";
 import "./index.scss";
 
-export default function Technologies() {
+interface Props {
+  number: number;
+  handleTechnology: (number: number) => void;
+  name: string;
+  description: string;
+  image: {
+    landscape: string;
+    portrait: string;
+  };
+}
+
+export default function Technologies({
+  handleTechnology,
+  number,
+  name,
+  description,
+  image,
+}: Props) {
   return (
     <section id="technologies">
       <section className="text-side">
         <ul className="control">
-          <li className="active">1</li>
-          <li>2</li>
-          <li>3</li>
+          <li
+            className={number === 1 ? "active" : ""}
+            onClick={() => handleTechnology(1)}
+          >
+            1
+          </li>
+          <li
+            className={number === 2 ? "active" : ""}
+            onClick={() => handleTechnology(2)}
+          >
+            2
+          </li>
+          <li
+            className={number === 3 ? "active" : ""}
+            onClick={() => handleTechnology(3)}
+          >
+            3
+          </li>
         </ul>
         <section className="about">
           <h1>The technology...</h1>
-          <h2>Launch vehicle</h2>
-          <p>
-            A launch vehicle or carrier rocket is a rocket-propelled vehicle
-            used to carry a payload from Earth's surface to space, usually to
-            Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful
-            in operation. Standing 150 metres tall, it's quite an awe-inspiring
-            sight on the launch pad!
-          </p>
+          <h2>{name}</h2>
+          <p>{description}</p>
         </section>
       </section>
       <section className="img-side">
         <picture>
-          <source media="(min-width: 1200px)" srcSet={imgPortrait} />
-          <img src={imgLandscape} alt="" loading="lazy" />
+          <source media="(min-width: 1200px)" srcSet={image.portrait} />
+          <img src={image.landscape} alt="" loading="lazy" />
         </picture>
       </section>
     </section>
--- src/pages/technology/index.tsx
+++ src/pages/technology/index.tsx
@@ -1,8 +1,62 @@
+import { useState } from "react";
 import Navbar from "../../components/navbar";
 import Technologies from "../../components/technologies";
 import "./index.scss";
 
 export default function Technology() {
+  const [technology, setTechnology] = useState(1);
+
+  const handleTechnology = (number: number) => {
+    setTechnology(number);
+  };
+
+  const renderTechnology = () => {
+    if (technology === 1) {
+      return (
+        <Technologies
+          handleTechnology={handleTechnology}
+          number={technology}
+          name="Launch vehicle"
+          image={{
+            portrait:
+              "../../assets/technology/image-launch-vehicle-portrait.jpg",
+            landscape:
+              "../../assets/technology/image-launch-vehicle-landscape.jpg",
+          }}
+          description="A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!"
+        />
+      );
+    } else if (technology === 2) {
+      return (
+        <Technologies
+          handleTechnology={handleTechnology}
+          number={technology}
+          name="Spaceport"
+          image={{
+            portrait: "../../assets/technology/image-spaceport-portrait.jpg",
+            landscape: "../../assets/technology/image-spaceport-landscape.jpg",
+          }}
+          description="A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch."
+        />
+      );
+    } else if (technology === 3) {
+      return (
+        <Technologies
+          handleTechnology={handleTechnology}
+          number={technology}
+          name="Space capsule"
+          image={{
+            portrait:
+              "../../assets/technology/image-space-capsule-portrait.jpg",
+            landscape:
+              "../../assets/technology/image-space-capsule-landscape.jpg",
+          }}
+          description="A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained."
+        />
+      );
+    }
+  };
+
   return (
     <section id="technology">
       <Navbar />
@@ -11,7 +65,7 @@ export default function Technology() {
           <span className="number">03</span>Space Lanuch 101
         </h1>
 
-        <Technologies />
+        {renderTechnology()}
       </section>
     </section>
   );
